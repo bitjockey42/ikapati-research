06dc0f895e76a291ff074d2d3f849c763531ae63
Allyson Julian <bitjockey@jackpoint.network>
2020-01-22 00:26:26 -0800

    Take out utility functions from make dataset
    Former-commit-id: 006b1e7b7f9f0d5c1f015c6f07aed488b5291111

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -1,7 +1,22 @@
-from tensorflow import keras
+import argparse
+import os
+import json
 
 import numpy as np
+from tensorflow import keras
 
 
 def convert_prediction(prediction, num_classes):
     return keras.utils.to_categorical(np.argmax(prediction, axis=0), num_classes)
+
+
+def model(filename):
+    return keras.models.load_model(filename)
+
+
+def _parse_args():
+    parser = argparse.ArgumentParser()
+
+
+if __name__ == "__main__":
+    pass
