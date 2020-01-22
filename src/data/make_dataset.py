b47a461fd798757976b7c609cbc655b78ae226df
Allyson Julian <bitjockey@jackpoint.network>
2020-01-22 00:26:26 -0800

    Take out utility functions from make dataset
    Former-commit-id: 9e4f33d85200aa7ece8b3919851dd11a605e36f0

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -14,6 +14,9 @@ import numpy as np
 from tensorflow import keras
 from sklearn.model_selection import train_test_split
 
+from src.data.image_processing import preprocess_image
+from src.data import utils
+
 logger = logging.getLogger(__name__)
 
 
@@ -25,20 +28,20 @@ def prepare_dataset(data_dir, output_dir, species=None, file_ext="JPG"):
     labels = []
     diseases = {}
 
-    folder_paths = get_folder_paths(data_dir, species)
+    folder_paths = utils.get_folder_paths(data_dir, species)
 
     for label_id, folder_path in enumerate(folder_paths):
-        species, disease = get_species_disease(folder_path)
+        species, disease = utils.get_species_disease(folder_path)
         diseases[label_id] = disease
-        filenames = find_image_files(folder_path, file_ext=file_ext)
+        filenames = utils.find_image_files(folder_path, file_ext=file_ext)
         for filename in filenames:
             labels.append(label_id)
             preprocessed_image = preprocess_image(filename)
             preprocessed_images.append(preprocessed_image)
 
-    classes = get_classes(labels)
+    classes = utils.get_classes(labels)
     num_classes = len(classes)
-    labels_enc = convert_to_one_hot_labels(labels, num_classes)
+    labels_enc = utils.convert_to_one_hot_labels(labels, num_classes)
 
     train_data, test_data, train_labels_enc, test_labels_enc = train_test_split(preprocessed_images, labels_enc, test_size=0.2, random_state=13)
     train_data, eval_data, train_labels_enc, eval_labels_enc = train_test_split(train_data, train_labels_enc, test_size=0.2, random_state=13)
@@ -54,89 +57,6 @@ def prepare_dataset(data_dir, output_dir, species=None, file_ext="JPG"):
         "eval_labels": eval_labels_enc,
         "test_labels": test_labels_enc,
     }
-
-
-def get_folder_paths(data_dir, species=None):
-    if species:
-        return sorted(glob.glob(os.path.join(data_dir, f"{species}*/")))
-    return sorted(os.listdir(data_dir))
-
-
-def get_species_disease(folder_path):
-    folder_name = os.path.basename(os.path.dirname(folder_path))
-    species, disease = folder_name.split("___")
-    return species, disease
-
-
-def find_image_files(folder_path, file_ext="JPG"):
-    """ Find image files with file_ext in data_dir """
-    return glob.glob(os.path.join(folder_path, f"*.{file_ext}"))
-
-
-def get_classes(labels):
-    """ Get the classes """
-    return np.unique(labels)
-
-
-def convert_to_one_hot_labels(labels, num_classes):
-    """ Convert labels (e.g. "2") to one hot encoding (e.g [0, 0, 1])"""
-    return keras.utils.to_categorical(labels, num_classes=num_classes)
-
-
-def preprocess_images(filenames):
-    """ Load and preprocess images before feeding into CNN """
-    return np.asarray(list(map(preprocess_image, filenames)))
-
-
-def preprocess_image(filename, normalize=True, standardize=True):
-    image = load_image(filename)
-    pixel_values = convert_to_pixel_values(image, normalize, standardize)
-    pixel_values.reshape(-1, image.size[0], image.size[1], 1)
-    return pixel_values
-
-
-def load_image(filename):
-    """ Load image and optionaly split into different channels """
-    image = Image.open(filename)
-    return image
-
-
-def get_image_filepath(data_dir, row):
-    """ Get image filepath from row """
-    return os.path.join(data_dir, f"{row.Species}___{row.Label}", row.Filename)
-
-
-def convert_to_pixel_values(image, normalize, standardize):
-    """ Convert image to array of pixel values """
-    pixels = np.asarray(image)
-    if normalize:
-        pixels = normalize_pixel_values(pixels)
-    if standardize:
-        pixels = standardize_pixel_values(pixels)
-    return pixels
-
-
-def normalize_pixel_values(pixels):
-    """ Normalize pixel values to be in the range [0, 1] """
-    pixels = pixels.astype('float32')
-    pixels /= 255.0
-    return pixels
-
-
-def standardize_pixel_values(pixels):
-    """ Globally standardize pixel values to positive """
-    mean, std = pixels.mean(), pixels.std()
-    pixels = (pixels - mean) / std
-    pixels = np.clip(pixels, -1.0, 1.0)
-    pixels = (pixels + 1.0) / 2.0
-    return pixels
-
-
-def save_dataset(output_dir, dataset):
-    """ Save dataset as *.npy to """
-    for key, data in dataset.items():
-        file_path = os.path.join(output_dir, f"{key}.npy")
-        np.save(file_path, data)
 
 
 @click.command()
@@ -157,7 +77,7 @@ def main(data_dir, output_dir, species, file_ext):
         os.makedirs(output_dir)
 
     dataset = prepare_dataset(data_dir, output_dir, species, file_ext)
-    save_dataset(output_dir, dataset)
+    utils.save_dataset(output_dir, dataset)
 
 
 if __name__ == '__main__':
