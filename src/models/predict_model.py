2611ec56baf867a045466730d52b069b6a738b35
Allyson Julian <bitjockey@jackpoint.network>
2020-01-22 18:37:40 -0800

    Delete old models and convert models to tflite
    Former-commit-id: 19662cb146ae2ba61734f7c6e0e893f11bd906ca

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -12,42 +12,54 @@ import json
 import click
 
 import numpy as np
+import tensorflow as tf
 from tensorflow import keras
 
 from src.data.image_processing import preprocess_image
 
 
-def get_classes(classes_filename):
-    with open(classes_filename) as f:
-        classes = json.load(f)
-    return classes
+def get_labels(filename):
+    with open(filename, "r") as f:
+        labels = f.readlines()
+        return list(map(str.strip, labels))
 
 
 def get_label_id(prediction):
-    label_id = np.argmax(prediction)
-    return str(label_id)
+    return np.argmax(prediction)
 
 
-def load_model(filename):
-    return keras.models.load_model(filename)
-
-
-def predict(predictor, input_data, classes):
-    predictions = predictor.predict(np.array([input_data]))
-    prediction = predictions[0]
-    return classes[get_label_id(prediction)]
+def load_tflite_model(filename):
+    interpreter = tf.lite.Interpreter(model_path=filename)
+    interpreter.allocate_tensors()
+    return interpreter
 
 
 @click.command()
 @click.argument('model_filename', type=click.Path())
-@click.option('--image-filename', type=click.Path())
-@click.option('--classes-filename', type=click.Path())
-def main(model_filename, image_filename, classes_filename):
-    classes = get_classes(classes_filename)
-    predictor = load_model(model_filename)
-    input_data = preprocess_image(image_filename, normalize=True, standardize=True)
-    prediction = predict(predictor, input_data, classes)
-    print(f"CLASS: {prediction}")
+@click.option('--input-filename', type=click.Path())
+@click.option('--labels-filename', type=click.Path())
+def main(model_filename, input_filename, labels_filename):
+    # Load labels
+    labels = get_labels(labels_filename)
+
+    # Load saved tflite model
+    interpreter = load_tflite_model(model_filename)
+
+    # Preprocess image
+    input_data = preprocess_image(input_filename, normalize=True, standardize=True)
+
+    # Get input and output tensors.
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+
+    # Make a prediction
+    interpreter.set_tensor(input_details[0]['index'], np.array([input_data]))
+    interpreter.invoke()
+    predictions = interpreter.get_tensor(output_details[0]['index'])
+    label_id = get_label_id(predictions[0])
+
+    # Show result
+    print(f"Detected: {labels[label_id]}")
 
 
 if __name__ == '__main__':
