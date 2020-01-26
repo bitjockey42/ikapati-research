abaaec1584d6ec857f61ed0a10edf266293ffe28
Allyson Julian <bitjockey@jackpoint.network>
2020-01-26 00:48:58 -0800

    Delete old models, and write metadata for models
    Former-commit-id: 3d445aa0f614a50e0daf6fae54be1d18c7ed4cff

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -13,6 +13,7 @@ import os
 import numpy as np
 import json
 
+from datetime import datetime
 from uuid import uuid4
 
 import tensorflow as tf
@@ -85,17 +86,17 @@ def _get_callbacks(monitor, model_dir, model_id):
             filepath=os.path.join(model_dir, model_id, "{epoch}.h5"),
             save_best_only=True,
             monitor=monitor,
-            verbose=1
+            verbose=1,
         ),
     ]
 
 
-def _load_dataset(data_dir, dataset_name, batch_size, num_classes):
+def load_dataset(data_dir, dataset_name, batch_size, num_classes):
     record_file_path = os.path.join(data_dir, f"{dataset_name}.tfrecord")
     return io.read_dataset(record_file_path, batch_size, num_classes)
 
 
-def _save_model(model, model_dir, model_id):
+def save_model(model, model_dir, model_id):
     print(f"Saving model {model_id}")
     model_filepath = os.path.join(model_dir, model_id, "final.h5")
     model.save(model_filepath)
@@ -105,9 +106,68 @@ def _save_model(model, model_dir, model_id):
     tflite_model = converter.convert()
 
     with open(
-        os.path.join(model_dir, f"{model_id}.tflite"), "wb"
+        os.path.join(model_dir, model_id, "model.tflite"), "wb"
     ) as converted_model_file:
         converted_model_file.write(tflite_model)
+
+
+def train(train_dir, model_dir, batch_size, epochs, monitor):
+    # Load metadata
+    metadata_file_path = os.path.join(train_dir, "metadata.json")
+    metadata = io.read_metadata(metadata_file_path)
+    model_id = metadata["id"]
+    num_classes = metadata["num_classes"]
+
+    # Create directories
+    model_dir_path = os.path.join(model_dir, model_id)
+    if not os.path.exists(model_dir_path):
+        os.makedirs(model_dir_path)
+
+    # Load data
+    train_dataset = load_dataset(train_dir, "train", batch_size, num_classes)
+    eval_dataset = load_dataset(train_dir, "eval", batch_size, num_classes)
+
+    # Create the model
+    classifier = model(num_classes)
+
+    # Steps
+    steps = metadata["file_counts"]["train"] // batch_size
+    validation_steps = metadata["file_counts"]["eval"] // batch_size
+
+    # Train
+    history = classifier.fit(
+        train_dataset,
+        epochs=epochs,
+        steps_per_epoch=steps,
+        validation_data=eval_dataset,
+        validation_steps=validation_steps,
+        callbacks=_get_callbacks(monitor, model_dir, model_id),
+    )
+
+    return classifier, history, model_id
+
+
+def write_metadata(model_dir, model_id, batch_size, epochs, monitor):
+    print("Write metadata for model")
+    metadata_file_path = os.path.join(model_dir, model_id, "metadata.json")
+    current_datetime = datetime.utcnow()
+
+    metadata = {
+        "id": model_id,
+        "created_date": current_datetime.strftime("%Y-%m-%d %T%z"),
+        "arguments": {"batch_size": batch_size, "epochs": epochs, "monitor": monitor},
+    }
+
+    with open(metadata_file_path, "w") as json_file:
+        json.dump(metadata, json_file)
+
+    return metadata
+
+
+def evaluate(model, dataset, batch_size, file_count):
+    steps = file_count // batch_size
+    loss, acc = model.evaluate(dataset, verbose=0, steps=steps)
+    return loss, acc
 
 
 def _parse_args():
@@ -141,48 +201,6 @@ def _parse_args():
     return parser.parse_known_args()
 
 
-def train(train_dir, model_dir, batch_size, epochs, monitor):
-    # Load metadata
-    metadata_file_path = os.path.join(train_dir, "metadata.json")
-    metadata = io.read_metadata(metadata_file_path)
-    model_id = metadata["id"]
-    num_classes = metadata["num_classes"]
-
-    # Create directories
-    model_dir_path = os.path.join(model_dir, model_id)
-    if not os.path.exists(model_dir_path):
-        os.makedirs(model_dir_path)
-
-    # Load data
-    train_dataset = _load_dataset(train_dir, "train", batch_size, num_classes)
-    eval_dataset = _load_dataset(train_dir, "eval", batch_size, num_classes)
-
-    # Create the model
-    classifier = model(num_classes)
-
-    # Steps
-    steps = metadata["file_counts"]["train"] // batch_size
-    validation_steps = metadata["file_counts"]["eval"] // batch_size
-
-    # Train
-    history = classifier.fit(
-        train_dataset,
-        epochs=epochs,
-        steps_per_epoch=steps,
-        validation_data=eval_dataset,
-        validation_steps=validation_steps,
-        callbacks=_get_callbacks(monitor, model_dir, model_id),
-    )
-
-    return classifier, history, model_id
-
-
-def evaluate(model, dataset, batch_size, file_count):
-    steps = file_count // batch_size 
-    loss, acc = model.evaluate(dataset, verbose=0, steps=steps)
-    return loss, acc
-
-
 if __name__ == "__main__":
     args, unknown = _parse_args()
 
@@ -195,4 +213,13 @@ if __name__ == "__main__":
     )
 
     # save model
-    _save_model(classifier, args.model_dir, model_id)
+    save_model(classifier, args.model_dir, model_id)
+
+    # write metadata
+    write_metadata(
+        model_dir=args.model_dir,
+        model_id=model_id,
+        batch_size=args.batch_size,
+        epochs=args.epochs,
+        monitor=args.monitor,
+    )
