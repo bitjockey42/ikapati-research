a908b4d9c51737afcab0a0ea35dbc51f8dcb530a
Allyson Julian <bitjockey@jackpoint.network>
2020-01-26 12:37:58 -0800

    Add visualization tools
    Former-commit-id: b936ff5ddfc48a71dcf7d2234ce555083eb63311

---
+++ src/visualization/visualize.py
@@ -0,0 +1,43 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+from typing import List
+from matplotlib import pyplot as plt
+
+
+def create_metrics_for_dataset_type(history: List[dict], dataset_type: str) -> pd.DataFrame:
+    loss_key = "loss"
+    accuracy_key = "accuracy"
+    
+    if dataset_type == "validation":
+        loss_key = "val_loss"
+        accuracy_key = "val_accuracy"
+    
+    data = {
+        "loss": history[loss_key],
+        "accuracy": history[accuracy_key]
+    }
+    
+    df = pd.DataFrame(data)
+    df["epoch"] = df.index
+    df["name"] = dataset_type
+    
+    return df
+
+
+def combine_dataframes(data_frames: List[pd.DataFrame]):
+    combined_df = pd.concat(data_frames)
+    combined_df = combined_df.reset_index()
+    return combined_df
+
+
+def create_metrics_dataframe(history: List[dict]) -> pd.DataFrame:
+    training_df = create_metrics_for_dataset_type(history, "train")
+    validation_df = create_metrics_for_dataset_type(history, "validation")
+
+    return combine_dataframes([training_df, validation_df])
+
+
+def learning_curves(metric: str, metrics_df: pd.DataFrame):
+    return sns.lineplot(x="epoch", y=metric, hue="name", data=metrics_df, legend="full")
