69a38e49d6a1bc7bb7064b150a8e8ee880378183
Allyson Julian <bitjockey@jackpoint.network>
2020-01-26 16:58:39 -0800

    Add comment
    Former-commit-id: aa95a7503fe253892c10bd246f4cf7d3a51d9630

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -36,10 +36,13 @@ from tensorflow.keras import (
 from src.data import io
 
 
-def model(num_classes, learning_rate=0.1, activation="linear"):
-    """Generate a simple model"""
-    padding = "same"
+def model(num_classes, learning_rate=0.1, activation="linear", padding="same"):
+    """
+    This is a convolutional neural network based on the AlexNet implementation here:
+    https://engmrk.com/alexnet-implementation-using-keras/
+    """
     print(f"learning_rate: {learning_rate} - activation: {activation}")
+
     model = keras.Sequential(
         [
             # Layer 1
@@ -47,20 +50,44 @@ def model(num_classes, learning_rate=0.1, activation="linear"):
                 96,
                 kernel_size=(11, 11),
                 input_shape=(256, 256, 3),
-                strides=(4,4),
+                strides=(4, 4),
                 padding=padding,
                 activation=activation,
             ),
             layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding=padding),
             # Layer 2
-            layers.Conv2D(256, kernel_size=(11, 11), strides=(2, 2), activation=activation, padding=padding),
+            layers.Conv2D(
+                256,
+                kernel_size=(11, 11),
+                strides=(2, 2),
+                activation=activation,
+                padding=padding,
+            ),
             layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding=padding),
             # Layer 3
-            layers.Conv2D(384, kernel_size=(3, 3), strides=(2, 2), activation=activation, padding=padding),
+            layers.Conv2D(
+                384,
+                kernel_size=(3, 3),
+                strides=(2, 2),
+                activation=activation,
+                padding=padding,
+            ),
             # Layer 4
-            layers.Conv2D(384, kernel_size=(3, 3), strides=(1, 1), activation=activation, padding=padding),
+            layers.Conv2D(
+                384,
+                kernel_size=(3, 3),
+                strides=(1, 1),
+                activation=activation,
+                padding=padding,
+            ),
             # Layer 5
-            layers.Conv2D(256, kernel_size=(3, 3), strides=(1, 1), activation=activation, padding=padding),
+            layers.Conv2D(
+                256,
+                kernel_size=(3, 3),
+                strides=(1, 1),
+                activation=activation,
+                padding=padding,
+            ),
             layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding=padding),
             # Pass to Fully Connected (FC) Layers
             layers.Flatten(),
