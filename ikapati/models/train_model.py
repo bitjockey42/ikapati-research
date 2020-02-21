9349eff40ccf62c72d53570557bca91ec9c0386e
BitJockey <bitjockey@jackpoint.network>
2020-02-20 17:54:56 -0800

    Add workaround for the cudnn issue

--- ikapati/models/train_model.py
+++ ikapati/models/train_model.py
@@ -305,6 +305,20 @@ def _parse_args():
 
 
 def main():
+    # Workaround for issue with cuDNN not loading https://github.com/tensorflow/tensorflow/issues/24828#issuecomment-576778701
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+
+    if gpus:
+        try:
+            # Currently, memory growth needs to be the same across GPUs
+            for gpu in gpus:
+                tf.config.experimental.set_memory_growth(gpu, True)
+            logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+            print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+        except RuntimeError as e:
+            # Memory growth must be set before GPUs have been initialized
+            print(e)
+
     args = _parse_args()
     start_time = datetime.utcnow().strftime("%Y-%m-%d__%H_%M%S")
 
